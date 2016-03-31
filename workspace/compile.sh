cee6c51eb2bfe169f67d8620caa9fdf630963140
Tais Bellini <tais38@gmail.com>
2016-03-31 07:53:26 -0300

    fix compile.sh file

--- workspace/compile.sh
+++ workspace/compile.sh
@@ -4,10 +4,11 @@ pushd JavaCCPajeParser/src/br/ufrgs/inf/tlbellini/
 javacc PajeGrammar.jj
 popd
 pushd JavaCCPajeParser/src/
+export CLASSPATH=../..//include/mysql-connector-java-5.1.38-bin.jar:.
 javac br/ufrgs/inf/tlbellini/lib/*.java
 javac br/ufrgs/inf/tlbellini/*.java
 popd
-echo
 export CLASSPATH=`pwd`/JavaCCPajeParser/src/
+export CLASSPATH=$CLASSPATH:/Users/taisbellini/TCC/aiyra/workspace/include/mysql-connector-java-5.1.38-bin.jar:.
 echo "Execute the program by typing:"
 echo "$ java br.ufrgs.inf.tlbellini.PajeGrammar <path to input Paje tracefile>"
