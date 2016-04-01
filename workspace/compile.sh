c1dcf25dc666ca1c0c0c4ce13396cac58f272550
Lucas Schnorr <schnorr@inf.ufrgs.br>
2016-04-01 12:31:21 -0300

    make also work in any Linux (if you run source compile.sh from the workspace directory)

--- workspace/compile.sh
+++ workspace/compile.sh
@@ -9,6 +9,6 @@ javac br/ufrgs/inf/tlbellini/lib/*.java
 javac br/ufrgs/inf/tlbellini/*.java
 popd
 export CLASSPATH=`pwd`/JavaCCPajeParser/src/
-export CLASSPATH=$CLASSPATH:/Users/taisbellini/TCC/aiyra/workspace/include/mysql-connector-java-5.1.38-bin.jar:.
+export CLASSPATH=$CLASSPATH:/Users/taisbellini/TCC/aiyra/workspace/include/mysql-connector-java-5.1.38-bin.jar:.:`pwd`/./include/mysql-connector-java-5.1.38-bin.jar
 echo "Execute the program by typing:"
 echo "$ java br.ufrgs.inf.tlbellini.PajeGrammar <path to input Paje tracefile>"
